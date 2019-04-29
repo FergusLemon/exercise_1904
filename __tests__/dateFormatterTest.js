f636d05532899c9ff5df10537e5edfb1efc02226
Fergus Lemon <fergus.lemon@gmail.com>
2019-04-29 12:14:40 +0100

    Implement formatDate function and add more tests.
    Why:
    
    To test the functionality required and implement it.
    
    This change addresses the need by:
    
    Testing the correct format is returned and one error handling test case.
    Implement the required functionality using two methods from the date-fns
    library.

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,8 +1,22 @@
-import {format} from '../src/dateFormatter';
+import {formatDate} from '../src/dateFormatter';
+
+const January = 0; //js Date object month is indexed from 0
+const December = 11;
 
 it('formats today as "TODAY"', () => {
-  const December = 11; //js Date object month is indexed from 0
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime();
-  expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+  expect(formatDate(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+});
+
+it('formats all other days as "DD/MM/YYYY"', () => {
+  const systemDateTime = new Date(2018, January, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2018, January, 16, 12, 50).getTime();
+  expect(formatDate(dateTimeToFormat, systemDateTime)).toBe('16/01/2018');
+});
+
+it('returns "Invalid Date" when unconvertable values are passed', () => {
+  const systemDateTime = new Date(2018, January, 15, 10, 5).getTime();
+  const dateTimeToFormat = "Invalid string to compare with valid date";
+  expect(formatDate(dateTimeToFormat, systemDateTime)).toBe('Invalid Date');
 });
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,7 @@
-export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+const { format, isSameDay } = require('date-fns');
+
+export const formatDate = (dateToFormatTimeMillis, systemDateTimeMillis) => {
+  return isSameDay(dateToFormatTimeMillis, systemDateTimeMillis)
+    ? 'TODAY'
+    : format(dateToFormatTimeMillis, 'DD/MM/YYYY');
 };
